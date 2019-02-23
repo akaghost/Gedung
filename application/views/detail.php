1470402d999fd14d8ae1b5fc537b07b172b7e9c5
aguskurdi <agus.kurdi24@gmail.com>
2019-02-24 00:39:35 +0700

    second init

---
+++ application/views/detail.php
@@ -0,0 +1,70 @@
+	<section id="inner-headline">
+      <div class="container">
+        <div class="row">
+          <div class="span4">
+            <div class="inner-heading">
+              <h2>Partner Detail</h2>
+            </div>
+          </div>
+          <div class="span8">
+            <ul class="breadcrumb">
+              <li><a href="<?php echo base_url(); ?>"><i class="icon-home"></i></a><i class="icon-angle-right"></i></li>
+              <li><a href="<?php echo base_url('main/partners'); ?>">Partners</a><i class="icon-angle-right"></i></li>
+              <li class="active">Partner detail</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="content">
+      <div class="container">
+        <div class="row">
+          <div class="span8">
+            <article>
+              <div class="top-wrapper">
+                <div class="post-heading">
+                  <h3><a href="#">The City</a></h3>
+                </div>
+                <!-- start flexslider -->
+                <div class="flexslider">
+                  <ul class="slides">
+                    <li>
+                      <img src="<?php echo base_url('assets/'); ?>img/works/full/image-01-full.jpg" alt="" />
+                    </li>
+                    <li>
+                      <img src="<?php echo base_url('assets/'); ?>img/works/full/image-02-full.jpg" alt="" />
+                    </li>
+                    <li>
+                      <img src="<?php echo base_url('assets/'); ?>img/works/full/image-03-full.jpg" alt="" />
+                    </li>
+                  </ul>
+                </div>
+                <!-- end flexslider -->
+              </div>
+              <p>
+                Qui ut ceteros comprehensam. Cu eos sale sanctus eligendi, id ius elitr saperet, ocurreret pertinacia pri an. No mei nibh consectetuer, semper laoreet perfecto ad qui, est rebum nulla argumentum ei. Fierent adipisci iracundia est ei, usu timeam persius
+                ea. Usu ea justo malis, pri quando everti electram ei, ex homero omittam salutatus sed.
+              </p>
+            </article>
+          </div>
+          <div class="span4">
+            <aside class="right-sidebar">
+              <div class="widget">
+                <h5 class="widgetheading">Partner Information</h5>
+                <ul class="folio-detail">
+                  <li><label>Category :</label> Gedung Serbaguna</li>
+                  <li><label>Target :</label> Wedding, Farwell Party, Graduation, Party Mobile Legend</li>
+                  <li><label>Location :</label> <a href="https://goo.gl/maps/5cHnT7JYbs52" target="_blank">https://goo.gl/maps/5cHnT7JYbs52</a></li>                  
+                </ul>
+              </div>
+              <div class="widget">
+                <h5 class="widgetheading">Text widget</h5>
+                <p>
+                  Lorem ipsum dolor sit amet, unum suscipiantur te cum, vide magna ea eam. At eos wisi tractatos temporibus. Ne has omnis harum. Ei mea graece delenit nominati. Ut dolore albucius torquatos vel, choro gubergren no mel.
+                </p>
+              </div>
+            </aside>
+          </div>
+        </div>
+      </div>
+    </section>
---
+++ application/views/partners.php
@@ -0,0 +1,131 @@
+    <section id="inner-headline">
+		<div class="container">
+			<div class="row">
+				<div class="span4">
+					<div class="inner-heading">
+						<h2>Our <strong>Partners</strong></h2>
+					</div>
+				</div>
+				<div class="span8">
+					<ul class="breadcrumb">
+						<li><a href="<?php echo base_url(); ?>"><i class="icon-home"></i></a><i class="icon-angle-right"></i></li>              
+						<li class="active">Partners</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+    </section>
+    <section id="content">
+		<div class="container">
+			<div class="row">
+				<div class="span12">					
+					<div class="row">
+						<section id="projects">
+							<ul id="thumbs" class="portfolio">
+								<!-- Item Project and Filter Name -->
+								<li class="item-thumbs span3 design" data-id="id-0" data-type="web">
+									<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+									<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Portfolio name" href="<?php echo base_url('assets/'); ?>img/works/full/image-01-full.jpg">
+										<span class="overlay-img"></span>
+										<span class="overlay-img-thumb font-icon-plus"></span>
+									</a>
+									<!-- Thumb Image and Description -->
+									<img src="<?php echo base_url('assets/'); ?>img/works/thumbs/image-01.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
+								</li>
+								<!-- End Item Project -->
+								<!-- Item Project and Filter Name -->
+								<li class="item-thumbs span3 design" data-id="id-1" data-type="icon">
+									<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+									<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Portfolio name" href="<?php echo base_url('assets/'); ?>img/works/full/image-02-full.jpg">
+										<span class="overlay-img"></span>
+										<span class="overlay-img-thumb font-icon-plus"></span>
+									</a>
+									<!-- Thumb Image and Description -->
+									<img src="<?php echo base_url('assets/'); ?>img/works/thumbs/image-02.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
+								</li>
+								<!-- End Item Project -->
+								<!-- Item Project and Filter Name -->
+								<li class="item-thumbs span3 photography" data-id="id-2" data-type="graphic">
+									<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+									<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Portfolio name" href="<?php echo base_url('assets/'); ?>img/works/full/image-03-full.jpg">
+										<span class="overlay-img"></span>
+										<span class="overlay-img-thumb font-icon-plus"></span>
+									</a>
+									<!-- Thumb Image and Description -->
+									<img src="<?php echo base_url('assets/'); ?>img/works/thumbs/image-03.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
+								</li>
+								<!-- End Item Project -->
+								<!-- Item Project and Filter Name -->
+								<li class="item-thumbs span3 design" data-id="id-0" data-type="web">
+									<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+									<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Portfolio name" href="<?php echo base_url('assets/'); ?>img/works/full/image-04-full.jpg">
+										<span class="overlay-img"></span>
+										<span class="overlay-img-thumb font-icon-plus"></span>
+									</a>
+									<!-- Thumb Image and Description -->
+									<img src="<?php echo base_url('assets/'); ?>img/works/thumbs/image-04.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
+								</li>
+								<!-- End Item Project -->
+								<!-- Item Project and Filter Name -->
+								<li class="item-thumbs span3 photography" data-id="id-4" data-type="web">
+									<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+									<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Portfolio name" href="<?php echo base_url('assets/'); ?>img/works/full/image-05-full.jpg">
+										<span class="overlay-img"></span>
+										<span class="overlay-img-thumb font-icon-plus"></span>
+									</a>
+									<!-- Thumb Image and Description -->
+									<img src="<?php echo base_url('assets/'); ?>img/works/thumbs/image-05.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
+								</li>
+								<!-- End Item Project -->
+								<!-- Item Project and Filter Name -->
+								<li class="item-thumbs span3 photography" data-id="id-5" data-type="icon">
+									<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+									<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Portfolio name" href="<?php echo base_url('assets/'); ?>img/works/full/image-06-full.jpg">
+										<span class="overlay-img"></span>
+										<span class="overlay-img-thumb font-icon-plus"></span>
+									</a>
+									<!-- Thumb Image and Description -->
+									<img src="<?php echo base_url('assets/'); ?>img/works/thumbs/image-06.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
+								</li>
+								<!-- End Item Project -->
+								<li class="item-thumbs span3 design" data-id="id-0" data-type="web">
+									<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+									<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Portfolio name" href="<?php echo base_url('assets/'); ?>img/works/full/image-07-full.jpg">
+										<span class="overlay-img"></span>
+										<span class="overlay-img-thumb font-icon-plus"></span>
+									</a>
+									<!-- Thumb Image and Description -->
+									<img src="<?php echo base_url('assets/'); ?>img/works/thumbs/image-07.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
+								</li>
+								<!-- End Item Project -->
+								<!-- Item Project and Filter Name -->
+								<li class="item-thumbs span3 design" data-id="id-0" data-type="graphic">
+									<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+									<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Portfolio name" href="<?php echo base_url('assets/'); ?>img/works/full/image-08-full.jpg">
+										<span class="overlay-img"></span>
+										<span class="overlay-img-thumb font-icon-plus"></span>
+									</a>
+									<!-- Thumb Image and Description -->
+									<img src="<?php echo base_url('assets/'); ?>img/works/thumbs/image-08.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
+								</li>
+								<!-- End Item Project -->
+							</ul>
+						</section>
+						<div align="center">					
+							<div class="bs-docs-example">
+							  <div class="pagination">
+								<ul>
+								  <li class="disabled"><a href="#">Prev</a></li>
+								  <li class="active"><a href="#">1</a></li>
+								  <li><a href="#">2</a></li>
+								  <li><a href="#">3</a></li>
+								  <li><a href="#">Next</a></li>
+								</ul>
+							  </div>
+							</div>					
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+    </section>
